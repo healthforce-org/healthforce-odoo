239426ab54fe32c31b6ae1759ad66fef389c6499
b45 <b45@dr.com>
2025-05-28 21:57:02 +0700

    feat: init healthfoce odoo project docker

---
+++ odoo_addons/sdmk_core/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'SDMK Core',
+    'version': '1.0',
+    'summary': 'Modul core untuk SDMK',
+    'category': 'Custom',
+    'author': 'Achmad Dinda',
+    'depends': ['base'],
+    'data': [
+        'views/some_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
